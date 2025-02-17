c90db95df085b280f9585bf80ac0e7a0a4f03f07
RuoRuo <790395245@qq.com>
2025-02-17 12:24:45 +0800

    修改iconfont

--- themeConfig.mjs
+++ themeConfig.mjs
@@ -83,8 +83,8 @@ export const themeConfig = {
         {
           crossorigin: "anonymous",
           rel: "stylesheet",
-          // href: "https://www.rz15.cn/file/iconfont.css",
-          href: "https://cdn2.codesign.qq.com/icons/g5ZpEgx3z4VO6j2/latest/iconfont.css",
+          href: "https://www.rz15.cn/file/iconfont.css",
+          // href: "https://cdn2.codesign.qq.com/icons/g5ZpEgx3z4VO6j2/latest/iconfont.css",
         },
       ],
       // Embed code
