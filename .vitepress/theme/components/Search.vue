ec88932764089d650fa383eb92c99513988946d1
RuoRuo <790395245@qq.com>
2025-02-19 22:21:47 +0800

    update algolia

--- .vitepress/theme/components/Search.vue
+++ .vitepress/theme/components/Search.vue
@@ -12,7 +12,7 @@
       :future="{
         preserveSharedStateOnUnmount: true,
       }"
-      index-name="imsyy"
+      index-name="ruoge_pages"
       @state-change="searchChange"
     >
       <ais-configure :hits-per-page.camel="8" />
